ee24c9ee4a210a5f0b0f2e938fc4d54eb9fd0a68
Voloshenko Evgeniu <95745884+Shnipper@users.noreply.github.com>
2022-05-15 19:59:13 +0300

    Initial Commit

---
+++ UIKitSliderGame/UIKitSliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitSliderGameApp.swift
+//  UIKitSliderGame
+//
+//  Created by Евгений Волошенко on 15.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct UIKitSliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
